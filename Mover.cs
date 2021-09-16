25c13fc6008ca03cdf84c3e63d5f271ba1b29455
z00diq <45630610+z00diq@users.noreply.github.com>
2021-09-16 11:45:28 +0300

    Add files via upload

---
+++ Mover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-_speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_speed * Time.deltaTime,0,0);
+        }
+    }
+}
